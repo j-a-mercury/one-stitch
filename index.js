9821bf89a5b4582855d4f3332c2b3e60c00cc984
J. A. Mercury <jeremiah-a-mercury@gmail.com>
2015-12-19 14:07:29 -0500

    real Hello World

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hellow World')
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hellow World!');
+});
+
+var server = app.listen(80, funciton() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
