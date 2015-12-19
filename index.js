57c5a062b94f21ebf502a38cfd693090e8779210
J. A. Mercury <jeremiah-a-mercury@gmail.com>
2015-12-19 14:45:41 -0500

    CAPITALIZATION matters

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res) {
 	res.send('Hello World!');
 });
 
-var portaroo = process.env.port || 3000
+var portaroo = process.env.PORT || 3000
 
 var server = app.listen(portaroo, function() {
 	var host = server.address().address;
