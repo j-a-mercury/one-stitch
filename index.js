e72bfd2992f0f5d5c7499ae2ce4959363a70798a
J. A. Mercury <j-a-mercury@users.noreply.github.com>
2016-01-16 18:01:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 
+app.get('*.json', function(req, res) {
+	res.sendfile('app' + req.path);
+});
+
+app.get('generate.js', function(req, res) {
+	var fs = require('fs');
+	fs.writeFile('data.json', 'Hey there!', function(err) {
+    		if(err) {
+        		return console.log(err);
+    		}
+	});
+});
+
+app.get('*', function(req, res) {
+	res.sendfile('index.html');
+});
+
 app.use(express.static(__dirname));
 
 var portaroo = process.env.PORT || 3000
